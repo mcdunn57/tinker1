df833251d408baeba014f95486d3c616e7f4ef53
mcdunn57 <mcdunn57@hotmail.com>
2017-03-12 15:48:12 -0400

    initial

---
+++ sec_get.py
@@ -0,0 +1,25 @@
+﻿import wget
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+#will take url in call and then identify the .gz to download
+
+
+def getFiles2(url):
+    r  = requests.get("https://" +url)
+    data = r.text
+    z=[]
+    try:
+        soup = BeautifulSoup(data)
+        for link in soup(text=re.compile(r'.gz')):
+            z.append(link.string)
+        for i in z:
+            site='https://'+url+i
+            print(site)
+            wget.download(site)
+    except:
+        print("Try Again")
+  
+getFiles2('www.sec.gov/Archives/edgar/Feed/2017/QTR1/')
